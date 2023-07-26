5126aea0091f61e0a3dd5a4eaf1c50038935c708
김도윤 <113526669+doyuny@users.noreply.github.com>
2023-07-26 10:52:01 +0900

    인트타입 나누기 오류수정

--- Sources/Options/MenuOption.swift
+++ Sources/Options/MenuOption.swift
@@ -8,7 +8,7 @@ protocol MenuOption: Option, Menu {}
 
 extension MenuOption {
     func info() -> String {
-        let price = String(format: "%.1f", self.price / 1000)
+        let price = String(format: "%.1f", Double(self.price) / 1000.0)
         return "\(id). \(name)\t\t\t | W \(price) | \(desc)"
     }
 }
